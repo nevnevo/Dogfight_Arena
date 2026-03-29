679ea0f550bdd80448bf950c5ec9a858f9f8adad
naftul <naftulmezada@013.net>
2026-03-29 19:17:09 +0300

    testing thread soloution

--- Dogfight_Arena/Objects/GameObject.cs
+++ Dogfight_Arena/Objects/GameObject.cs
@@ -7,13 +7,8 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI;
-
 using Windows.UI.Xaml;
-
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Shapes;
-
 
 namespace Dogfight_Arena.Objects
 {
@@ -23,55 +18,82 @@ namespace Dogfight_Arena.Objects
         public double _y;
         protected Image _objectImage;
         protected Canvas _field;
-        
+
         public bool Colisional { get; set; } = true;
+
+        // Constructor
         public GameObject(double x, double y, string fileName, Canvas field, double size)
         {
             _x = x;
             _y = y;
-            _objectImage = new Image();
-            _objectImage.Width = size; 
+            _objectImage = new Image
+            {
+                Width = size
+            };
             _field = field;
-            SetImage(fileName);
-            _field.Children.Add(_objectImage);
-            Render();
+
+            // Schedule UI initialization on main thread
+            _ = InitializeAsync(fileName);
         }
 
-        private void SetImage(string fileName)
+        // Thread-safe initialization
+        private async Task InitializeAsync(string fileName)
         {
-            _objectImage.Source = new BitmapImage(new Uri($"ms-appx:///Assets/{fileName}"));
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher
+                .RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    _objectImage.Source = new BitmapImage(new Uri($"ms-appx:///Assets/{fileName}"));
+                    _field.Children.Add(_objectImage);
+                    Canvas.SetLeft(_objectImage, _x);
+                    Canvas.SetTop(_objectImage, _y);
+                });
         }
+
+        // Rect with angle (can override)
         public virtual Rect Rect(int angle)
         {
-
             return new Rect(_x, _y, _objectImage.Width - 15, _objectImage.Height - 15);
-
         }
+
+        // Default Rect
         public virtual Rect Rect()
         {
-            
             return new Rect(_x, _y, _objectImage.Width - 15, _objectImage.Height - 15);
+        }
 
-        }
-        public virtual void Remove()
-        {
-            _field.Children.Remove(_objectImage);
-        }
+        // Thread-safe render
         public virtual async Task RenderAsync()
         {
-            await Windows.ApplicationModel.Core.CoreApplication
-                .MainView.CoreWindow.Dispatcher
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher
                 .RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
                     Canvas.SetLeft(_objectImage, _x);
                     Canvas.SetTop(_objectImage, _y);
                 });
         }
+
+        // Fire-and-forget render
         public virtual void Render()
         {
             _ = RenderAsync();
-
         }
+
+        // Thread-safe remove
+        public virtual void Remove()
+        {
+            _ = RemoveAsync();
+        }
+
+        private async Task RemoveAsync()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher
+                .RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    _field.Children.Remove(_objectImage);
+                });
+        }
+
+        // Override for collision behavior
         public virtual void Collide(GameObject otherObject) { }
     }
 }
