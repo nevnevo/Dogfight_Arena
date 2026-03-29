dd959ad2b1c2be26ec8da16ed310872b4a672a08
naftul <naftulmezada@013.net>
2026-03-29 17:13:41 +0300

    fix

--- Dogfight_Arena/Services/GameManager.cs
+++ Dogfight_Arena/Services/GameManager.cs
@@ -279,7 +279,7 @@ namespace Dogfight_Arena.Services
 
             LocalPlayer = null;
             SecondPlayer = null;
-            client.Delete()
+            client.Delete();
             client = new Client(42069);
             GameEvents = new Events();
 
